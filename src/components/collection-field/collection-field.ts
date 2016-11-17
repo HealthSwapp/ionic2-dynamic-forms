2c2d397025d250da8ffd41d9527b34a4f749fb98
eafif <healthswappdev@gmail.com>
2016-11-17 17:44:39 -0500

    ability to add to a collection

--- src/components/collection-field/collection-field.ts
+++ src/components/collection-field/collection-field.ts
@@ -1,7 +1,8 @@
 import { Component, Input } from '@angular/core';
-import { FormGroup} from '@angular/forms';
+import { FormGroup, FormArray} from '@angular/forms';
 
 import { DynamicField } from '../../models/dynamic-field';
+import { DynamicForm } from '../../providers/dynamic-form';
 
 @Component({
   selector: 'collection-field',
@@ -10,5 +11,18 @@ import { DynamicField } from '../../models/dynamic-field';
 export class CollectionFieldComponent {
   @Input() field: DynamicField = {};
   @Input() form: FormGroup;
+
+  constructor(
+    public df: DynamicForm
+    ) { }
+
+  addToCollection = (fields: DynamicField[]) => {
+    console.log('adding fields');
+
+    let x = this.df.toFormGroup(fields);
+    console.log(x);
+
+    (<FormArray>this.form.controls[this.field.name]).push(x)
+  }
 }
 
